8e262f9b4b694d7ac414e7db954ead5dfe544aff
Anurag Koul <koulanurag@gmail.com>
2021-07-04 12:33:28 -0700

    feat: vdn (#1)
    - developed value decomposition network
    - tested on Switch2-v1 and Switch4-v1

--- README.md
+++ README.md
@@ -3,17 +3,22 @@ Minimal implementation of multi-agent reinforcement learning algorithms(marl). T
 
 ## Installation
 ```bash 
-  pip install ma-gym pytorch
+  pip install ma-gym pytorch wandb
 ```
 
 ## Usage
 ```bash
-python <algo_name>.py # such as `maddpg.py`
+python <algo_name>.py # such as `vdn.py`
 ```
+
+## Algorithms
+
+- [x] VPN (Value Prediction Network)
+- [x] MADDPG (Multi Agent Deep Deterministic Policy Gradient)
 
 ## Contributing
 Contributions are always welcome! 
 
-Feel free to send a `pull-request` if you
-would like to add a new algorithm or further optimize an existing algorithm.
-If adding a new algorithm, it will be nice if it could be tested with ma-gym to maintain consistency.
+Feel free to send a `pull-request` if you would like to add a new algorithm or further optimize an existing algorithm. 
+Also, If adding a new algorithm, please start by raising an `issue` and it will be nice if it could be tested with 
+`ma-gym` to maintain consistency.
--- vdn.py
+++ vdn.py
@@ -1,12 +1,14 @@
-import gym
 import collections
 import random
 
+import gym
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import numpy as np
+
+USE_WANDB = True  # if enabled, logs data on wandb server
 
 
 class ReplayBuffer:
@@ -44,12 +46,11 @@ class QNet(nn.Module):
         self.num_agents = len(observation_space)
         for agent_i in range(self.num_agents):
             n_obs = observation_space[agent_i].shape[0]
-            n_action = action_space[agent_i].n
             setattr(self, 'agent_{}'.format(agent_i), nn.Sequential(nn.Linear(n_obs, 128),
                                                                     nn.ReLU(),
                                                                     nn.Linear(128, 64),
                                                                     nn.ReLU(),
-                                                                    nn.Linear(64, n_action)))
+                                                                    nn.Linear(64, action_space[agent_i].n)))
 
     def forward(self, obs):
         q_values = [torch.empty(obs.shape[0], )] * self.num_agents
@@ -67,8 +68,8 @@ class QNet(nn.Module):
         return action
 
 
-def train(q, q_target, memory, optimizer, gamma, batch_size):
-    for i in range(10):
+def train(q, q_target, memory, optimizer, gamma, batch_size, update_iter=10):
+    for _ in range(update_iter):
         s, a, r, s_prime, done_mask = memory.sample(batch_size)
 
         q_out = q(s)
@@ -76,7 +77,7 @@ def train(q, q_target, memory, optimizer, gamma, batch_size):
         sum_q = q_a.sum(dim=1, keepdims=True)
         max_q_prime = q_target(s_prime).max(dim=2)[0].squeeze(-1)
         target = r.sum(dim=1, keepdims=True) + gamma * (max_q_prime * done_mask).sum(dim=1, keepdims=True)
-        loss = F.smooth_l1_loss(sum_q, target)
+        loss = F.smooth_l1_loss(sum_q, target.detach())
 
         optimizer.zero_grad()
         loss.backward()
@@ -88,11 +89,8 @@ def test(env, num_episodes, q):
     for episode_i in range(num_episodes):
         state = env.reset()
         done = [False for _ in range(env.n_agents)]
-
         while not all(done):
-            env.render()
-            action = q.sample_action(torch.Tensor(state).unsqueeze(0), epsilon=0)
-            action = action[0].data.cpu().numpy().tolist()
+            action = q.sample_action(torch.Tensor(state).unsqueeze(0), epsilon=0)[0].data.cpu().numpy().tolist()
             next_state, reward, done, info = env.step(action)
             score += np.array(reward)
             state = next_state
@@ -100,46 +98,47 @@ def test(env, num_episodes, q):
     return sum(score / num_episodes)
 
 
-def main(env_name, lr, gamma, batch_size):
+def main(env_name, lr, gamma, batch_size, buffer_limit, log_interval, max_episodes,
+         max_epsilon, min_epsilon, test_episodes, warm_up_steps, update_iter):
     env = gym.make(env_name)
     test_env = gym.make(env_name)
-    memory = ReplayBuffer(buffer_limit=50000)
+    memory = ReplayBuffer(buffer_limit)
 
     q = QNet(env.observation_space, env.action_space)
     q_target = QNet(env.observation_space, env.action_space)
     q_target.load_state_dict(q.state_dict())
-
-    score = np.zeros(env.n_agents)
-    log_interval = 20
-
     optimizer = optim.Adam(q.parameters(), lr=lr)
 
-    for episode_i in range(1000):
-        epsilon = max(0.1, 0.9 - 0.1 * (episode_i / 600))  # Linear annealing
+    score = np.zeros(env.n_agents)
+    for episode_i in range(max_episodes):
+        epsilon = max(min_epsilon, max_epsilon - (max_epsilon - min_epsilon) * (episode_i / (0.4 * max_episodes)))
         state = env.reset()
         done = [False for _ in range(env.n_agents)]
-
-        env.render()
+        step_i = 0
         while not all(done):
-            action = q.sample_action(torch.Tensor(state).unsqueeze(0), epsilon)
-            action = action[0].data.cpu().numpy().tolist()
+            action = q.sample_action(torch.Tensor(state).unsqueeze(0), epsilon)[0].data.cpu().numpy().tolist()
             next_state, reward, done, info = env.step(action)
-            memory.put((state, action, (np.array(reward)).tolist(), next_state,
-                        np.array(done, dtype=int).tolist()))
+            step_i += 1
+            if step_i >= env._max_steps or (step_i < env._max_steps and not all(done)):
+                _done = [False for _ in done]
+            else:
+                _done = done
+            memory.put((state, action, (np.array(reward)).tolist(), next_state, np.array(_done, dtype=int).tolist()))
             score += np.array(reward)
-            state = next_state
-            env.render()
 
-        if memory.size() > 2000:
-            train(q, q_target, memory, optimizer, gamma, batch_size)
+            state = next_state
+
+        if memory.size() > warm_up_steps:
+            train(q, q_target, memory, optimizer, gamma, batch_size, update_iter)
 
         if episode_i % log_interval == 0 and episode_i != 0:
             q_target.load_state_dict(q.state_dict())
-            test_score = test(test_env, 5, q)
-            print("# of episode :{}, avg train score : {:.1f}, "
-                  "test score: {:.1f} n_buffer : {}, eps : {:.1f}%".format(episode_i,
-                                                                           sum(score / log_interval), test_score,
-                                                                           memory.size(), epsilon * 100))
+            test_score = test(test_env, test_episodes, q)
+            print("#{:<10}/{} episodes , avg train score : {:.1f}, test score: {:.1f} n_buffer : {}, eps : {:.1f}"
+                  .format(episode_i, max_episodes, sum(score / log_interval), test_score, memory.size(), epsilon))
+            if USE_WANDB:
+                wandb.log({'episode': episode_i, 'test-score': sum(score / log_interval),
+                           'buffer-size': memory.size(), 'epsilon': epsilon, 'train-score': sum(score / log_interval)})
             score = np.zeros(env.n_agents)
 
     env.close()
@@ -147,7 +146,21 @@ def main(env_name, lr, gamma, batch_size):
 
 
 if __name__ == '__main__':
-    main(env_name='ma_gym:Switch2-v1',
-         lr=0.0005,
-         batch_size=32,
-         gamma=0.99)
+    kwargs = {'env_name': 'ma_gym:Switch2-v1',
+              'lr': 0.0005,
+              'batch_size': 32,
+              'gamma': 0.99,
+              'buffer_limit': 50000,
+              'log_interval': 20,
+              'max_episodes': 10000,
+              'max_epsilon': 0.9,
+              'min_epsilon': 0.1,
+              'test_episodes': 5,
+              'warm_up_steps': 2000,
+              'update_iter': 10}
+    if USE_WANDB:
+        import wandb
+
+        wandb.init(project='minimal-marl', config={'algo': 'vdn', **kwargs})
+
+    main(**kwargs)
