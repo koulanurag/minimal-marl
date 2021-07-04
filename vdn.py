eb9b8e1ae0c75f704cd5b2ff7f68d02dbab66cc5
Anurag Koul <koulanurag@gmail.com>
2021-07-03 21:10:09 -0700

    fix

--- vdn.py
+++ vdn.py
@@ -146,20 +146,20 @@ def main(env_name, lr, gamma, batch_size, buffer_limit, log_interval,
 
 
 if __name__ == '__main__':
-    env_name = 'ma_gym:Switch2-v1'
+    kwargs = {'env_name': 'ma_gym:Switch2-v1',
+              'lr': 0.0005,
+              'batch_size': 32,
+              'gamma': 0.99,
+              'buffer_limit': 50000,
+              'log_interval': 20,
+              'max_episodes': 10000,
+              'max_epsilon': 0.9,
+              'min_epsilon': 0.1,
+              'test_episodes': 5,
+              'warm_up_steps': 2000}
     if USE_WANDB:
         import wandb
 
-        wandb.init(project='minimal-marl', config={'algo': 'vdn', 'env_name': env_name})
+        wandb.init(project='minimal-marl', config={'algo': 'vdn', **kwargs})
 
-    main(env_name=env_name,
-         lr=0.0005,
-         batch_size=32,
-         gamma=0.99,
-         buffer_limit=50000,
-         log_interval=20,
-         max_episodes=10000,
-         max_epsilon=0.9,
-         min_epsilon=0.1,
-         test_episodes=5,
-         warm_up_steps=2000)
+    main(**kwargs)
